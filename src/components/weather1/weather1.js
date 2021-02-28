4039645cafb3c1c2105b36d134c0164f473faca5
がっちゃん <yuki.dolphin.koga@gmail.com>
2021-03-01 00:03:40 +0900

    ✨ slotを使わずにDOMの注入を試す

--- src/components/weather1/weather1.js
+++ src/components/weather1/weather1.js
@@ -1,7 +1,7 @@
 class MyButton extends HTMLElement {
   constructor() {
       super();
-      this.innerHTML = `<button>Hello</button>`;
+      this.innerHTML = `<button>${this.innerHTML}</button>`;
   }
 }
 customElements.define("my-button", MyButton);
